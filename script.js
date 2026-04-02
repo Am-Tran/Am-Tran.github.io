78e59e482e140ea34d6e4341724abe619f273081
Am-Tran <amelie.ny.tran@gmail.com>
2026-04-02 16:06:59 +0200

    Modif carrousel

--- script.js
+++ script.js
@@ -2,10 +2,12 @@
 const swiper = new Swiper(".mySwiper", {
     loop: true,
     autoHeight: true,
+    preventClicks: false,
+    preventClicksPropagation: false,
     pagination: { el: ".swiper-pagination", clickable: true },
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
-   
+
 });
